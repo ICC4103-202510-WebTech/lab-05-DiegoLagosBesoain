2be9c61b9739bdd11358073c64c5a32116a8dd50
DiegoLagosBesoain <dnlagos@miuandes.cl>
2025-04-09 15:21:40 -0400

    entregando

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,33 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |i|
+    User.create!(
+        email: "user#{i+1}@xd.cl",
+        first_name: "User #{i+1}",
+        last_name: "Last #{i+1}"
+        
+    )
+end
+10.times do |i|
+    receiver_id = i+2
+    if i == 9
+        receiver_id = 1
+    end
+    Chat.create!(
+        sender_id: i+1,
+        receiver_id: receiver_id
+        
+        
+    )
+end
+10.times do |i|
+    
+    Message.create!(
+        chat_id: i+1,
+        user_id: i+1,
+        body: "Message #{i+1}"
+    )
+end
+
+
